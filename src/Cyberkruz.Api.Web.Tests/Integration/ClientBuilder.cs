d994e2fbf36e539b3a8fa4a0176c9f09c21b65db
Matt Kruskamp <cyberkruz@gmail.com>
2017-03-01 21:59:46 -0700

    Updated the tests to have integration categorized tests.

---
+++ src/Cyberkruz.Api.Web.Tests/Integration/ClientBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Cyberkruz.Api.Web.Tests.Integration
+{
+    public class ClientBuilder
+    {
+        public static HttpServer BuildServer()
+        {
+            var config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            var server = new HttpServer(config);
+            return server;
+        }
+
+        public static HttpRequestMessage BuildRequest(string location, HttpMethod method)
+        {
+            var request = new HttpRequestMessage()
+            {
+                RequestUri = new Uri("http://localhost" + location)
+            };
+            request.Headers.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            return request;
+        }
+
+        public static HttpClient BuildClient()
+        {
+            var server = BuildServer();
+            var client = new HttpClient(server);
+            return client;
+        }
+    }
+}
---
+++ src/Cyberkruz.Api.Web.Tests/Integration/ValuesTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Cyberkruz.Api.Web.Tests.Integration
+{
+    [TestFixture(Category = "Integration")]
+    public class ValuesTests
+    {
+        [Test]
+        public async Task HelloWorldAsync()
+        {
+            var client = ClientBuilder.BuildClient();
+            var request = ClientBuilder.BuildRequest(
+                "/api/values/5", HttpMethod.Get);
+
+            var response = await client.SendAsync(request);
+
+            Assert.IsNotNull(response.Content);
+        }
+    }
+}
